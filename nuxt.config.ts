d05268b09fadcf72ca4932d408a9a79e788738de
zhangjianbin <2551490259@qq.com>
2025-08-28 17:11:27 +0800

    -

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     '@/assets/css/main.css', // 主样式文件
   ],
   plugins: [
-    '~/plugins/conditional-analytics.client.ts', // 条件加载 Google Analytics 插件
+    // '~/plugins/conditional-analytics.client.ts', // 条件加载 Google Analytics 插件
   ],
   runtimeConfig: {
     public: {
